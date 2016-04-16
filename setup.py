f2b09faf260733db6bd648c950371e7c2a574b54
Adam Knight <adam@movq.us>
2016-04-16 13:08:55 -0500

    Updated setup.py info

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -42,10 +42,11 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
-    keywords='key value store kvs',
+    keywords='mail maildir mailbox',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages.
